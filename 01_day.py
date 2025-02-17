fd78f00c64bc67750a5c066dc14241ea14193aba
Mastur <bikanerwalamastur13@gmail.com>
2025-02-17 09:07:38 +0530

    DAY 1 Add

---
+++ 01_day.py
@@ -0,0 +1,10 @@
+# syntax :
+# how to print a message in python
+
+print("Hello World","I Am devloper")
+
+print("Hello World","I Am devloper",sep="__")
+
+print("hello"); print("world")
+
+print("hello", end="\n"); print("world")
